1cbb4d91feb990f76f6ea10610cbd5fb17e42659
David N <gamma.dave@gmail.com>
2022-07-16 11:12:04 -0400

    rename viewsets to views

--- api_server/api/urls.py
+++ api_server/api/urls.py
@@ -1,20 +1,28 @@
 from django.urls import path, include
 
-from api import viewsets
+from api import views
 
 auth_views = [
-    path("login/", viewsets.LoginView.as_view(), name="auth-login"),
-    path("logout/", viewsets.LogoutView.as_view(), name="auth-logout"),
-    path("sign-up/", viewsets.MeViewset.as_view({"post": "create"})),
-    path("me/", viewsets.MeViewset.as_view({"put": "update", "get": "retrieve"})),
+    path("login/", views.LoginView.as_view(), name="auth-login"),
+    path("logout/", views.LogoutView.as_view(), name="auth-logout"),
+    path("sign-up/", views.MeViewset.as_view({"post": "create"})),
+    path("me/", views.MeViewset.as_view({"put": "update", "get": "retrieve"})),
 ]
 
 v1 = [
     path('auth/', include(auth_views)),
-    path("tasks/", viewsets.TaskViewset.as_view({"get": "list", "post": "create"}), name="tasks-list"),
-    path("tasks/<str:pk>/", viewsets.TaskViewset.as_view({"get": "retrieve", "put": "update"}), name="tasks-detail"),
-    path("task-lists/", viewsets.TaskListViewset.as_view({"get": "list", "post": "create"}), name="task-lists-list"),
-    path("task-lists/<str:pk>/", viewsets.TaskListViewset.as_view({"get": "retrieve", "put": "update"}), name="task-lists-list"),
+    path("tasks/", views.TaskViewset.as_view({"get": "list", "post": "create"}), name="tasks-list"),
+    path("tasks/<str:pk>/", views.TaskViewset.as_view({
+            "get": "retrieve",
+            "put": "update",
+            "patch": "partial_update",
+        }), name="tasks-detail"),
+    path("task-lists/", views.TaskListViewset.as_view({"get": "list", "post": "create"}), name="task-lists-list"),
+    path("task-lists/<str:pk>/", views.TaskListViewset.as_view({
+            "get": "retrieve",
+            "put": "update",
+            "patch": "partial_update",
+        }), name="task-lists-list"),
 ]
 
 urlpatterns = [
